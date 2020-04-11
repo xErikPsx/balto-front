97ad55f1aae1de3af0911790ef68596ab7d1a0b4
Erik Parra <pandiwix13@gmail.com>
2020-04-11 14:11:54 -0500

    Cambios en error 404

--- src/components/Pages/errors/Error404.jsx
+++ src/components/Pages/errors/Error404.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React from 'react';
+import ErrorDog from '../../../../src/sorry404Dog.png';
+import { Link } from 'react-router-dom';
 
-function Error404() {
-  return <div>This is the 404 Error</div>;
+function Error404Page() {
+  return (
+    <div className="error404Page">
+      <h2 className="error404Page__title">
+        ¡Ooops! Has encontrado el Error404
+      </h2>
+      <div className="error404Page__dogContainer">
+        <img src={ErrorDog} alt="" className="error404Page__dogImage" />
+      </div>
+      <h3 className="error404Page__messagge">
+        {' '}
+        Lo sentimos, pero la página que estás buscando no la podimos encontrar
+        <span> 🙁</span>
+      </h3>
+      <Link to="../" className="error404Page__link">
+        Haz clic en este enlace para volver al inicio
+      </Link>
+    </div>
+  );
 }
 
-export default Error404;
+export default Error404Page;
---
+++ src/containers/Errors/Error404Page.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Error404Page from '../../components/Pages/Errors/Error404';
+
+function Error404() {
+  return <Error404Page />;
+}
+
+export default Error404;
